217877129bcbf7b33fa589268aa76b112509e63c
Jade Humpherys <joshua.humpherys@protonmail.com>
2023-10-14 20:09:24 -0500

    start crate documentation and smol example

---
+++ examples/basic.rs
@@ -0,0 +1,37 @@
+use sassi::{SwarmAgent, SwarmManager};
+
+#[derive(Debug, PartialEq, Clone)]
+enum AgentInput {
+    Discrete(bool),
+    Fuzzy(f32),
+}
+#[derive(Debug, PartialEq, Clone, Default)]
+struct MyAgent {
+    data: Vec<f32>,
+}
+impl SwarmAgent for MyAgent {
+    const STATE_SIZE: usize = 5;
+    type Input = [AgentInput; 2];
+    type SpawnArgs = Vec<f32>;
+    fn spawn_new(args: &Self::SpawnArgs) -> Self {
+        MyAgent { data: args.clone() }
+    }
+    fn get_state(&self) -> [f32; Self::STATE_SIZE] {
+        let mut state: [f32; Self::STATE_SIZE] = [0.0; Self::STATE_SIZE];
+        for i in 0..Self::STATE_SIZE {
+            if i < self.data.len() {
+                state[i] = self.data[i];
+            } else {
+                state[i] = 0.0;
+            }
+        }
+        state
+    }
+    fn evaluate(&mut self, input: Self::Input) {
+        todo!()
+    }
+}
+
+fn main() {
+    let manager = SwarmManager::<MyAgent>::new();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
-#![warn(missing_docs)]
+#![warn(missing_docs, missing_doc_code_examples)]
 #![feature(generic_const_exprs, const_trait_impl)]
 
+//! A library for creating Swarm Intelligence AI Networks
+//!
+//! Pull requests are welcome!
+//!
+//! # Example
+//!
+#[doc = include_str!("../examples/basic.rs")]
+
 pub use agent::SwarmAgent;
-// pub use manager::SwarmManager;
+pub use manager::SwarmManager;
 
 mod agent;
 mod manager;
